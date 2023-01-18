8f8ebf3b18fe7024b64628a03eec9c88a2164aef
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-18 20:02:25 +0100

    code added

---
+++ webpack/src/App/index.tsx
@@ -0,0 +1,32 @@
+import { connect, MapDispatchToProps, MapStateToProps } from "react-redux";
+import { compose } from "redux";
+
+import { IGlobalStore } from "../../reducers/index";
+import { IActionCreator } from "../../interfaces/IReducers";
+
+import { fetchData } from "../../actions/MainActions";
+
+import App from "./App";
+
+
+
+interface IStateProps {
+    data: string[];
+}
+interface IDispatchProps {
+    fetchData: IActionCreator;
+}
+
+export type IApp = IStateProps & IDispatchProps;
+
+const mapStateToProps: MapStateToProps<IStateProps, {}, IGlobalStore> = ({main}) => ({
+    data: main.data,
+});
+
+const mapDispatchToProps: MapDispatchToProps<IDispatchProps, {}> = {
+    fetchData,
+};
+
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+)(App);
