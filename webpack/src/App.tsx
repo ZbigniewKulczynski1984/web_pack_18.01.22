2464facc808a98cdde74efb997f8594174a9ffa4
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-18 19:31:56 +0100

    code added

--- webpack/src/App.tsx
+++ webpack/src/App.tsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>My React App!</h1>
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
