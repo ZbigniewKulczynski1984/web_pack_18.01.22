48d39d473d6a511019e49815d59a2bd7a142a5a8
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-18 19:53:12 +0100

    code added

--- webpack/src/webpack.config.tsx
+++ webpack/src/webpack.config.tsx
@@ -1,37 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = {
+    distPath: path.join(__dirname, 'dist'),
+    srcPath: path.join(__dirname, 'src')
+};
+
+function srcPathExtend(subpath) {
+  return path.join(config.srcPath, subpath)
+}
 
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-      path: path.join(__dirname, '/dist'),
-      filename: 'index_bundle.js',
-      sourceMapFilename: 'index_bundle.map'
-  },
-  resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  devtool: 'inline-source-map',
-  module: {
-      rules: [
-          {
-              test: /\.tsx?$/,
-              use: ["babel-loader", "ts-loader", "tslint-loader"]
-            }
-          }
-      ]
-  },
-  {
-      test: /\.(ttf|eot|woff|woff2)$/,
-      use: {
-          loader: "file-loader",
-          options: {
-              name: "fonts/[name].[ext]",
-          },
-      },
-  }
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ];
+    entry: './src/index.tsx',
+    output: {
+        path: config.distPath,
+        filename: 'index_bundle.js',
+        sourceMapFilename: 'index_bundle.map'
+    },
+	
+    },
+    plugins: [
+        new CleanWebpackPlugin([config.distPath], {verbose: true}),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ];
