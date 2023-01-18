c50eb06d0369127620c0e2cf93157e99e7b7f31a
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-18 20:07:27 +0100

    code added

---
+++ webpack/src/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, Store, Middleware } from "redux";
+import createSagaMiddleware, { SagaMiddleware } from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+
+import reducer from "../reducers";
+import saga from "../sagas";
+
+const sagaMiddleware: SagaMiddleware<Middleware> = createSagaMiddleware();
+
+const store: Store = createStore(reducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(saga);
