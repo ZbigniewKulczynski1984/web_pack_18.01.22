7329463aef88177191cca0b6b81965442329189f
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-18 19:36:49 +0100

    code added

--- webpack/src/webpack.config.tsx
+++ webpack/src/webpack.config.tsx
@@ -2,29 +2,52 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+              },
+              {
+                  test: /\.scss$/,
+                  use: [
+                      {
+                          loader: "style-loader"
+                      },
+                      {
+                          loader: "css-loader",
+                          options: {
+                              sourceMap: true
+                          }
+                      },
+                      {
+                          loader: "sass-loader",
+                          options: {
+                              sourceMap: true
+                          }
+                  }]
+              }
+          ]
+      },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
-  ]
-};
+  ];
