e0bd98f94ae5bb16f8b94e00a29a24c3bd1be038
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-18 19:44:30 +0100

    code added

--- webpack/src/webpack.config.tsx
+++ webpack/src/webpack.config.tsx
@@ -2,51 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.tsx',
-    devtool: 'inline-source-map',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-              },
-              {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            plugins: [require('autoprefixer')()]
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-      },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    
-    ]
+  entry: './src/index.tsx',
+  output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'index_bundle.js',
+      sourceMapFilename: 'index_bundle.map'
   },
+  resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+  module: {
+      rules: [
+          {
+              test: /\.tsx?$/,
+              use: ["babel-loader", "ts-loader", "tslint-loader"]
+            }
+          }
+      ]
+  },
+  {
+      test: /\.(ttf|eot|woff|woff2)$/,
+      use: {
+          loader: "file-loader",
+          options: {
+              name: "fonts/[name].[ext]",
+          },
+      },
+  }
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
